a91de1c99bb2c8c4c8791eac3499d49324a67fab
filipa <filipadurao@gmail.com>
2017-12-04 08:50:58 +0000

    Finished Day 4 Parts 1 and 2!

---
+++ Day 4/Day 4 Part 1.cpp
@@ -0,0 +1,64 @@
+/*
+ * Day 4 Part 1.cpp
+ *
+ *  Created on: 04/12/2017
+ *      Author: filipa
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+bool checkRep(string input){
+
+	stringstream ss;
+	ss << input;
+
+	vector<string> words;
+	string word;
+
+	while(!ss.eof()){
+		ss >> word;
+		words.push_back(word);
+	}
+
+	sort(words.begin(), words.end());
+
+	for(unsigned int i = 0; i < words.size()-1; i++){
+		if(words.at(i) == words.at(i+1))
+			return false;
+	}
+
+	return true; 	// Is valid, no repetitions
+}
+
+
+int main(){
+
+	ifstream in_stream("Input Day 4.txt");
+	string input;
+	vector<string> inputs;
+
+	while(!in_stream.eof()){
+		getline(in_stream, input);
+		inputs.push_back(input);
+	}
+
+	unsigned int cont = 0;
+
+
+	for(unsigned int i = 0; i < inputs.size(); i++){
+		if(checkRep(inputs.at(i)))
+			cont ++;
+	}
+
+	cout << cont;
+
+	return 0;
+}
